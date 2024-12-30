ad93d16dca0ff68f5ccbee83a6c3926b898793cd
Michael Tuszynski <miketuszynski42@gmail.com>
2024-12-30 07:59:50 -0800

    Add proper types for NextAuth

--- src/components/SessionProvider.tsx
+++ src/components/SessionProvider.tsx
@@ -1,10 +1,11 @@
 "use client";
 
+import { Session } from "next-auth";
 import { SessionProvider as Provider } from "next-auth/react";
 
 type Props = {
   children: React.ReactNode;
-  session: any;
+  session: Session | null;
 };
 
 export function SessionProvider({ children, session }: Props) {
